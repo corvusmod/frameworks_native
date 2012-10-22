219e281c04020d4ca2a78b6e8e6815ceba2fd867
CorvusMod <corvusmod@home>
2012-10-22 16:09:48 +0200

    Revert: Come back to old surfaceflinger patches
    Change-Id: I01714d5f339fb3c8404082da0e5e59faf2e2d306

--- services/surfaceflinger/DisplayHardware/DisplayDispatcher.h
+++ services/surfaceflinger/DisplayHardware/DisplayDispatcher.h
@@ -17,57 +17,34 @@
 #ifndef _UI_DISPLAY_DISPATCHER_H
 #define _UI_DISPLAY_DISPATCHER_H
 
-#include <androidfw/Input.h>
-#include <androidfw/InputTransport.h>
-#include <utils/KeyedVector.h>
-#include <utils/Vector.h>
 #include <utils/threads.h>
 #include <utils/Timers.h>
-#include <utils/RefBase.h>
-#include <utils/String8.h>
-#include <utils/Looper.h>
-#include <utils/BitSet.h>
-#include <ui/DisplaySemaphore.h>
 #include <hardware/display.h>
 #include <hardware/hardware.h>
 #include <stddef.h>
 #include <unistd.h>
 #include <limits.h>
 #include <ui/DisplayCommand.h>
-#include "DisplaySemaphore.h"
->>>>>>> parent of 9643951... Come back to old surfaceflinger patches:services/surfaceflinger/DisplayHardware/DisplayDispatcher.h
+#include <ui/DisplaySemaphore.h>
 
-#define   DISPLAYDISPATCH_MAXBUFNO      3
-
-#define   DISPLAY_CMD_SETDISPPARA       0
-#define   DISPLAY_CMD_CHANGEDISPMODE    1
-#define   DISPLAY_CMD_OPENDISP          2
-#define   DISPLAY_CMD_CLOSEDISP         3
-#define   DISPLAY_CMD_GETHDMISTATUS     4
-#define   DISPLAY_CMD_GETTVSTATUS       5
-#define   DISPLAY_CMD_GETDISPPARA       6
-#define   DISPLAY_CMD_SETMASTERDISP     7
-#define   DISPLAY_CMD_GETMASTERDISP     8
-#define   DISPLAY_CMD_GETMAXWIDTHDISP   9
-#define   DISPLAY_CMD_GETMAXHDMIMODE    10
-#define   DISPLAY_CMD_GETDISPLAYMODE    11
-#define   DISPLAY_CMD_GETDISPCOUNT      12
-#define   DISPLAY_CMD_SETDISPMODE       13
-#define   DISPLAY_CMD_SETBACKLIGHTMODE  14
 namespace android 
 {
+	class SurfaceFlinger; 
+	
     /* 同显时的帧管理线程 */
     class DisplayDispatcherThread : public Thread 
     {
         public:
-            explicit DisplayDispatcherThread(display_device_t*	mDevice);
+            explicit DisplayDispatcherThread(display_device_t*	mDevice,const sp<SurfaceFlinger>& flinger);
             ~DisplayDispatcherThread();
             void                setSrcBuf(int srcfb_id,int srcfb_offset);
-            void                signalEvent();
+            void                signalEvent(int reason);
             void                waitForEvent();
             void 				resetEvent();
-
+            void				setConvertBufId(int bufid);
+            void				setConvertBufParam(int handle,int width,int height,int format);
         private:
+        	sp<SurfaceFlinger> mFlinger;
             sp<DisplaySemaphore>    mSemaphore;
             int                 mSrcfbid;
             int                 mSrcfboffset;
@@ -77,45 +54,71 @@ namespace android
             void				enqueuebuf(int frameidx);
             virtual bool        threadLoop();
             void 				LooperOnce();
-    		display_device_t*	mDispDevice;	
+    		display_device_t*	mDispDevice;
+    		int                 mWifiDisplayBufHandle;
+            int                 mWifiDisplayBufWidth;
+            int                 mWifiDisplayBufHeight;
+            int                 mWifiDisplayBufFormat;
+    		bool				mStartConvert;	
+    		int					mConvertBufId;
+    		int					mStartReason;
+    		int64_t				mStartTime;
+    		mutable Mutex       mBufferLock;
     };
 
     class DisplayDispatcher:public virtual RefBase
     {
         public:
-            DisplayDispatcher();
+            DisplayDispatcher(const sp<SurfaceFlinger>& flinger);
             ~DisplayDispatcher();
             
-            int     setDispProp(int cmd,int param0,int param1,int param2);
-            int     getDispProp(int cmd,int param0,int param1);
-            void    startSwapBuffer();
-        private:
-            int  	changeDisplayMode(int displayno, int value0,int value1);
-			int  	setDisplayParameter(int displayno, int value0,int value1);	
-			int  	setDisplayMode(int mode);
-			int		openDisplay(int displayno);
-			int 	closeDisplay(int displayno);
-			int 	getHdmiStatus(void);
-			int 	getTvDacStatus(void);
-			int 	getDisplayParameter(int displayno, int param);
-			int 	setMasterDisplay(int displayno);
-			int 	getMasterDisplay();
-			int 	getDisplayMode();
-			int 	getDisplayCount();
-			int		getMaxWidthDisplay();
-			int     getMaxHdmiMode();
-			int     setDisplayBacklightMode(int mode);
+            int     			setDispProp(int cmd,int param0,int param1,int param2);
+            int     			getDispProp(int cmd,int param0,int param1);
+            void    			startSwapBuffer();
+            	
+        private:   
+        	sp<SurfaceFlinger>  mFlinger; 			
+            int  				changeDisplayMode(int displayno, int value0,int value1);
+			int  				setDisplayParameter(int displayno, int value0,int value1);	
+			int  				setDisplayMode(int mode);
+			int					openDisplay(int displayno);
+			int 				closeDisplay(int displayno);
+			int 				getHdmiStatus(void);
+			int 				getTvDacStatus(void);
+			int 				getDisplayParameter(int displayno, int param);
+			int 				setMasterDisplay(int displayno);
+			int 				getMasterDisplay();
+			int 				getDisplayMode();
+			int 				getDisplayCount();
+			int					getMaxWidthDisplay();
+			int     			getMaxHdmiMode();
+			int     			setDisplayBacklightMode(int mode);   
+			int					setConvertBufId(int bufid);
+			int                 setDisplayOrientation(int orientation);
 			
-			bool 	mDisplayOpen0;
-	        bool 	mDisplayOpen1;
-	        int 	mDisplayMaster;
-	        int 	mDisplayMode;
-	        int 	mDisplayPixelFormat0;
-	        int 	mDisplayPixelFormat1;
-	        int 	mDisplayType0;
-	        int 	mDisplayType1;
-	        int 	mDisplayFormat0;
-	        int 	mDisplayFormat1;
+			/*wifi Display相关的处理*/
+			int     			startWifiDisplaySend(int displayno,int mode);
+			int     			endWifiDisplaySend(int displayno);
+			int     			startWifiDisplayReceive(int displayno,int mode);
+			int					endWifiDisplayReceive(int displayno);
+            int     			updateSendClient(int mode);
+			        			
+			bool 				mDisplayOpen0;
+	        bool 				mDisplayOpen1;
+	        int 				mDisplayMaster;
+	        int 				mDisplayMode;
+	        int 				mDisplayPixelFormat0;
+	        int 				mDisplayPixelFormat1;
+	        int 				mDisplayType0;
+	        int 				mDisplayType1;
+	        int 				mDisplayFormat0;
+	        int 				mDisplayFormat1;
+            int                 mWifiDisplayBufHandle;
+            int                 mWifiDisplayBufWidth;
+            int                 mWifiDisplayBufHeight;
+            int                 mWifiDisplayBufFormat;
+            bool				mStartConvert;
+            int					mConvertBufId;
             sp<DisplayDispatcherThread>   mThread;
             display_device_t*	mDevice;	
             
